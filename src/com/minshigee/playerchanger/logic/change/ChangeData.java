35751a3cb1cef238bd5660718fd14dee40b52029
MinShiGee <MinShiGee@gmail.com>
2021-10-23 15:58:11 +0900

    Change관련 모듈 수정

--- src/com/minshigee/playerchanger/logic/change/ChangeData.java
+++ src/com/minshigee/playerchanger/logic/change/ChangeData.java
@@ -4,6 +4,7 @@ import com.minshigee.playerchanger.PlayerChanger;
 import com.minshigee.playerchanger.domain.module.Data;
 import com.minshigee.playerchanger.util.MessageUtil;
 import com.mojang.datafixers.util.Pair;
+import org.bukkit.entity.Player;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
@@ -12,11 +13,13 @@ import java.util.ArrayList;
 public class ChangeData extends Data {
     private final ArrayList<Pair<Integer, Method>> changeMethods = new ArrayList<>();
 
-    public void addMethod(Integer code, Method method){
+    public void addMethod(Integer code, Method method)
+    {
+        MessageUtil.printConsoleLog("%s를 Change 목록에 추가합니다.".formatted(method.getName()));
         changeMethods.add(new Pair<>(code, method));
     }
 
-    public void excuteMethod(Integer code){
+    public void executeMethod(Integer code, Player p1, Player p2){
         changeMethods.stream()
                 .filter(pair -> pair.getFirst().equals(code))
                 .map(Pair::getSecond)
@@ -31,13 +34,14 @@ public class ChangeData extends Data {
                 );
     }
 
-    public void executeMethods(){
+    public void executeMethods(Player p1, Player p2){
         changeMethods.forEach(pair ->{
             int code = pair.getFirst();
             Method method = pair.getSecond();
             try {
-                method.invoke(PlayerChanger.getInstanceOfClass(method.getDeclaringClass()));
+                method.invoke(PlayerChanger.getInstanceOfClass(method.getDeclaringClass()), p1, p2);
             } catch (Exception e) {
+                MessageUtil.printConsoleLog(e.getMessage());
                 MessageUtil.printConsoleLog("change에 실패했습니다.");
             }
         });
--- src/com/minshigee/playerchanger/logic/change/ChangeRepository.java
+++ src/com/minshigee/playerchanger/logic/change/ChangeRepository.java
@@ -31,7 +31,7 @@ public class ChangeRepository extends Repository<ChangeData> {
         new BukkitRunnable(){
             @Override
             public void run() {
-                localDB.executeMethods();
+                changePlayerInfo();
             }
         }.runTaskLater(PlayerChanger.singleton, 60);
     }
@@ -40,31 +40,31 @@ public class ChangeRepository extends Repository<ChangeData> {
         localDB.addMethod(code, method);
     }
 
-    @MappingChange
+
     public void changePlayerInfo(){
         MessageUtil.printConsoleLog("아이템을 교체합니다.");
         ArrayList<Participant> pars = Util.getShuffledAliveParticipants();
         for(int i = 0; i < pars.size() / 2; i++){
             Player from = pars.get(i).getPlayer();
             Player to = pars.get(pars.size() - i - 1).getPlayer();
-            changeInventory(from,to);
-            changeLocation(from,to);
+            localDB.executeMethods(from,to);
         }
         if(pars.size() % 2 == 1){
             Player from = pars.get(pars.size()/2).getPlayer();
             Player to = pars.get(0).getPlayer();
-            changeInventory(from,to);
-            changeLocation(from,to);
+            localDB.executeMethods(from,to);
         }
     }
 
-    private void changeLocation(Player from, Player to){
+    @MappingChange
+    public void changeLocation(Player from, Player to){
         Location tmpLoc = from.getLocation();
         from.teleport(to.getLocation());
         to.teleport(tmpLoc);
     }
 
-    private void changeInventory(Player from, Player to){
+    @MappingChange
+    public void changeInventory(Player from, Player to){
         Inventory tmpInv = Bukkit.createInventory(null, InventoryType.PLAYER, "tmp Inventory");
         ItemStack[] tmpArmor = to.getInventory().getArmorContents();
         ItemStack[] tmpExtra = to.getInventory().getExtraContents();
